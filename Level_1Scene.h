479abf0457b73e4dfa0860a76d32830ee29bab5a
shuayp <2737320131@qq.com>
2016-07-16 00:27:24 +0800

    Add files via upload

--- Level_1Scene.h
+++ Level_1Scene.h
@@ -1,27 +1,23 @@
 #include "cocos2d.h"
-#include"Res.h"
 #include"HuanRun.h"
 USING_NS_CC;
 
 class Level_1Scene : public cocos2d::Layer
 {
 public:
+	Level_1Scene();
+	~Level_1Scene();
 	static cocos2d::Scene* createScence(); 
-	static CCPoint getPointWithType(ItemKindType type, ItemSizeType sizeType);
 
 	virtual bool onTouchBegan(cocos2d::Touch *touch, CCEvent *pEvent);
-	virtual void onTouchMoved(cocos2d::CCTouch *pTouch, CCEvent *pEvent);
-	virtual void ccTouchEnded(CCSet *pTouch, CCEvent *pEvent);
 
 	static bool iscollision(Sprite*spritel, Sprite*sprite2);
 	//void addItemWithType(ItemKindType type, int count);//添加的类型和数量
 	virtual bool init();
-	void checkupHip(float dt);
 
 
 	void update(float dt);
 	void update1(float dt);
-	void update2(float dt);
 
 	//void addItem();
 	void ReturnMain(Ref *ref);
@@ -36,6 +32,11 @@ public:
 	void onLeftPressed(Ref* ref);
 	void onDownPressed(Ref* ref);
 	void onRightPressed(Ref* ref);
+	void openmenu(Ref *ref);
+	void menurestart(Ref *ref);
+	void menuquit(Ref *ref);
+	void menuback(Ref *ref);
+	void NextScene(Ref *ref);
 
 	void ScoreRun();
 
@@ -43,13 +44,13 @@ public:
 	HuanRun*huanRun;
 	int TotalScore;
 
-	Label*               label1;
-	Label*               label;
-	float                   pTime;
+	Label* label1;
+	Label* label;
+	float pTime;
 
 	CCSize Winsize;
-	float rotateAngle;		//弧度
-	float addAngle;			//每次增加的弧度
+	float rotateAngle;//弧度
+	float addAngle;//每次增加的弧度
 	CCArray *ItemUnitArray;
 	CREATE_FUNC(Level_1Scene);
 
